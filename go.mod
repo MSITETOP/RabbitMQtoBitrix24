972e445b3d40ca99c91e48cfd0cb1afe2f1fc0d6
MSITE.TOP <admin@msite.top>
2023-01-15 18:26:23 +0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,10 @@
+module main
+
+go 1.19
+
+require (
+	github.com/isayme/go-amqp-reconnect v0.0.0-20210303120416-fc811b0bcda2 // indirect
+	github.com/rabbitmq/amqp091-go v1.5.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+	github.com/wagslane/go-rabbitmq v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+//export AMPQURL=amqp://kxhvlgcc:H8mE5QBUDMTbS56qlI3xIB6ex4NjIUqQ@jackal.rmq.cloudamqp.com/kxhvlgcc
+//export B24REST=https://ntp.t2.ipg4you.com/rest/
+
+package main
+
+import (
+    "bytes"
+    "io/ioutil"
+    "net/http"
+    "os"
+	"log"
+	"sync"
+
+	"github.com/streadway/amqp"
+	"github.com/isayme/go-amqp-reconnect/rabbitmq"
+)
+
+func main() {
+	rabbitmq.Debug = true
+    ampqURL := os.Getenv("AMPQURL") 
+    b24restURL := os.Getenv("B24REST") 
+    ampqIN := os.Getenv("AMPQIN") 
+    ampqOUT := os.Getenv("AMPQOUT") 
+    
+	conn, err := rabbitmq.Dial(ampqURL)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	consumeCh, err := conn.Channel()
+	if err != nil {
+		log.Panic(err)
+	}
+    
+    sendCh, err := conn.Channel()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	go func() {
+		d, err := consumeCh.Consume(
+            ampqIN, // queue
+            "",     // consumer
+            false,   // auto-ack
+            false,  // exclusive
+            false,  // no-local
+            false,  // no-wait
+            nil,    // args        
+        )
+		if err != nil {
+			log.Panic(err)
+		}
+
+		for msg := range d {
+            h := msg.Headers
+            method, _ := h["method"].(string)
+            token, _ := h["token"].(string)
+            user, _ := h["user"].(string)
+            message_id := msg.MessageId
+            //content_type := msg.ContentType
+            jsonData := []byte(string(msg.Body))
+            
+            log.Printf("msg id: %s", string(message_id))
+            log.Printf("msg method: %s", string(method))
+			log.Printf("msg request: %s", string(jsonData))
+            
+            if true {           //token && method && user && message_id && jsonData
+                urlAdr := b24restURL+"/"+user+"/"+token+"/"+method
+                resp, err := http.Post(urlAdr, "application/json", bytes.NewBuffer(jsonData))
+                if err != nil {
+                    log.Fatal(err)
+                }
+                jsonBody, _ := ioutil.ReadAll(resp.Body)
+                log.Printf("msg response: %s", string(jsonBody))
+
+                errsend := sendCh.Publish("", ampqOUT, false, false, amqp.Publishing{
+                    ContentType: "text/json",
+                    MessageId:   message_id,
+                    Body:        []byte(jsonBody),
+                }) 
+                if errsend != nil { 
+                    log.Printf("Error publish: %v", errsend)
+                }                
+            }          
+			msg.Ack(true)
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	wg.Wait()
+}
